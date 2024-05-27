02726a0b8fb08f799b7380c54cf68df8479054a0
NikitKhom <nikita.khomenko.n25@gmail.com>
2024-05-27 23:14:40 +0300

    update logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 function Navbar() {
     return (
         <nav className='navigation'>
-            <NavLink to='/create-button' className={({ isActive }) => `navigation__link ${isActive ?'navigation__link_active': ''}`}>Кнопка</NavLink>  
+            <NavLink to='/create-button' className={({ isActive }) => `navigation__link ${isActive ?'navigation__link_active': ''}`}>Button</NavLink>  
             <NavLink to='/create-input' className={({ isActive }) => `navigation__link ${isActive ?'navigation__link_active': ''}`}>Input</NavLink>  
-            <NavLink to='/create-form' className={({ isActive }) => `navigation__link ${isActive ?'navigation__link_active': ''}`}>Форма</NavLink>  
+            <NavLink to='/create-form' className={({ isActive }) => `navigation__link ${isActive ?'navigation__link_active': ''}`}>Form</NavLink>  
         </nav>
         
     )
